858a6351c24ab1c93b472b97a3d22c122941cd0f
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-11-24 17:38:49 +0530

    Did a little final look polishing like adding footer and text and background color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Calculator from './Calculator';
+import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <hr/>
-        <Calculator/>
+      <div className="App">
+        <div className="container">
+          <Calculator />
+          <hr/>
+          <p>This calculator app was built by me as a quick hack to practice my ReactJS skills. It is a capstone project undertaken by Nova Semita fellows</p>
+          <p>The calculator is a React Component that can be reused later in the future. Can be made embeddable to use within other webpages too.</p>
+          
+        </div>
+        <Footer />
       </div>
     );
   }
